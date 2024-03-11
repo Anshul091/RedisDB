b712a0ea4c95392e93b1f424c5dd691af1eeb92e
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-11 18:49:36 +0530

    Testing the test

--- resp.go
+++ resp.go
@@ -18,3 +18,24 @@ const (
 type Value struct {
 	typ   string
 	str   string
+	num   int
+	bulk  string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
