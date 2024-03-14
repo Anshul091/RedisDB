e3bb0af780c0279db8d99f9141af0b85960d43fd
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-14 16:55:00 +0530

    Obligatory placeholder commit message

--- handlers.go
+++ handlers.go
@@ -11,3 +11,9 @@ func ping(args []Value) Value {
 
 	return Value{typ: "string", str: args[0].bulk}
 }
+
+var Handlers = map[string]func([]Value) Value{
+	"PING": ping,
+	"SET":  set,
+	"GET":  get,
+	"HSET": hset,
