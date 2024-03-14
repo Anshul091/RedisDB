8bfbb4ef1a2a0c5d29510d09995fed0414e50d2a
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-15 00:03:28 +0530

    Commit committed

--- handlers.go
+++ handlers.go
@@ -24,3 +24,9 @@ var SETs = map[string]string{}
 var SETsMu = sync.RWMutex{}
 
 func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
