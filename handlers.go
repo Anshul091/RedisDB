9fa1f632dfffcb4ea4cb7536c052c05d419cb152
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-18 12:36:22 +0530

    Updated framework to the latest version

--- handlers.go
+++ handlers.go
@@ -63,3 +63,9 @@ func hset(args []Value) Value {
 	if len(args) != 3 {
 		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
 	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
