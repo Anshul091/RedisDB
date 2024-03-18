ee8a0e469075fe4457c4e2969abab474fe0933b9
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-19 05:17:22 +0530

    typo

--- handlers.go
+++ handlers.go
@@ -82,3 +82,10 @@ func hget(args []Value) Value {
 	if len(args) != 2 {
 		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
 	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
