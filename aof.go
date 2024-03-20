b35a6d0f2ced6a79c513a8158fff074f5da9edbb
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-21 02:33:46 +0530

    Updated

--- aof.go
+++ aof.go
@@ -10,3 +10,9 @@ import (
 type Aof struct {
 	file *os.File
 	rd   *bufio.Reader
+	mu   sync.Mutex
+}
+
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
