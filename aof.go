d1aae4da9e6e41da436c313e97528c5afbacc8af
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-21 05:29:05 +0530

    fix tpyo

--- aof.go
+++ aof.go
@@ -16,3 +16,9 @@ type Aof struct {
 func NewAof(path string) (*Aof, error) {
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
+		return nil, err
+	}
+
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
